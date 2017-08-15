c5b92d83aff450fd64c8019fbb9abed1a3ef8680
desireemiraldo <desimiraldo@gmail.com>
2017-08-15 16:14:45 -0300

    Antes de testar as combinações de features

--- Acelerometro_17_08_12.m
+++ Acelerometro_17_08_12.m
@@ -14,26 +14,40 @@ clear all; clc; close all
 
 Path = 'C:\Users\BMClab\Downloads\Desiree\Acelerometro\Acelerometro-GitHub\Coletas\';
 
-% Delsys data
-% Name = 'Acc_170803_EGS_';
-% Trial = {'1' '2' '3' '4' '5'}
-% ShankL = 2;ShankR = 3;
+%Delsys data
+
+Name = 'Acc_170803_EGS_';
+csv = '-Delsys 1.csv';
+Trial = {'1' '2' '3' '4' '5'};
+ShankL = 2;ShankR = 3;
+
 % Name = 'Acc_170731_RNW_';
 % csv = '-Delsys 1.csv';
 % Trial = {'1' '2' '4' '5'};
+%ShankL = 5;ShankR = 4;
 
-Name = 'Acc_170731_DCM_';
-csv = '-Delsys 1.csv';
-Trial = {'1' '2' '3' '4' };
-ShankL = 5;ShankR = 4;
+% Name = 'Acc_170731_DCM_';
+% csv = '-Delsys 1.csv';
+% Trial = {'1' '2' '3' '4' };
+% ShankL = 5;ShankR = 4;
 
-Fs = 148.39;
+Fs = 148.39; % Delsys sensor
+FsFP = 300;  % force plates
+
+
 ChannelType = 'AUX';
-Signal = {'IM ACC Pitch', 'IM ACC Roll', 'IM ACC Yaw'};
+Signal = {'IM ACC Pitch', 'IM ACC Roll', 'IM ACC Yaw',...
+    'IM GYR Pitch', 'IM GYR Roll', 'IM GYR Yaw'};
 
-Var = {'AccF','PitchF','RollF','YawF'};%,'AccF.^2'}; %Used in linear combination
+Var = {'ACCF','ACCPitchF','ACCRollF','ACCYawF',...
+    'GYR','GYRPitchF','GYRRollF','GYRYawF'};%,'AccF.^2'}; %Used in linear combination
+
 Sensors = {'ShankR','ShankL'};
 
+% -- Filter
+[n,Wn] = buttord(10/(Fs/2),20/(Fs/2),1,60);
+[b,a] = butter(n, Wn);
+% -- Linear combination
 p = zeros(ceil(Fs),length(Var),length(Sensors)*length(Trial));
 y = zeros(ceil(Fs),2*length(Trial));
 
@@ -42,12 +56,17 @@ for k = 1: length(Trial)
     File = [Name,Trial{k}];
     FilePath = [Path,File];
     
-    Pitch = ReadDelsys([FilePath,csv], ChannelType, Signal(1));
-    Roll = ReadDelsys([FilePath,csv], ChannelType, Signal(2));
-    Yaw = ReadDelsys([FilePath,csv], ChannelType, Signal(3));
+    for i = 1:length(Signal)
+        VarName = strrep(strrep(Signal{i},'IM ',''),' ','');
+        eval([VarName '= ReadDelsys([FilePath,csv], ChannelType, Signal(i));']);
+        temp = eval(VarName);
+        %Filtering
+        eval([VarName 'F' ' = [temp(:,1),filtfilt(b,a,temp(:,2:end))];']);
+    end
+    
     EMG = ReadDelsys([FilePath,csv], 'IMEMG', 'EMG');
     
-    cte = ones(size(Pitch,1),size(Pitch,2));
+    cte = ones(size(eval(VarName),1),size(eval(VarName),2));
     
     % Cortex data
     Forces = importdata([FilePath,'.forces']);
@@ -58,32 +77,15 @@ for k = 1: length(Trial)
         end
     end
     
-    % ANC file
-    % ANC = importdata([FileName,'.anc'],'\t',11);
-    %
-    % Accx1 = ANC.data(:,29);
-    % Accy1 = ANC.data(:,30);
-    % Accz1 = ANC.data(:,31);
-    % Accx2 = ANC.data(:,32);
-    % Accy2 = ANC.data(:,33);
-    % Accz2 = ANC.data(:,34);
-    %
-    % Acc1 = ANC.data(:,1);
-    
-    %% -- Filtering
-    
-    [n,Wn] = buttord(10/(Fs/2),20/(Fs/2),1,60);
-    [b,a] = butter(n, Wn);
-    
-    PitchF = [Pitch(:,1),filtfilt(b,a,Pitch(:,2:end))];
-    RollF = [Pitch(:,1),filtfilt(b,a,Roll(:,2:end))];
-    YawF = [Pitch(:,1),filtfilt(b,a,Yaw(:,2:end))];
-    
+
     %% -- Resultant
     
     % Delsys
-    Acc = [Pitch(:,1), sqrt(Pitch(:,2:end).^2 + Roll(:,2:end).^2 +Yaw(:,2:end).^2)];
-    AccF = [Pitch(:,1), sqrt(PitchF(:,2:end).^2 + RollF(:,2:end).^2 +YawF(:,2:end).^2)];
+    ACC = [ACCPitch(:,1), sqrt(ACCPitch(:,2:end).^2 + ACCRoll(:,2:end).^2 + ACCYaw(:,2:end).^2)];
+    ACCF = [ACCPitch(:,1), sqrt(ACCPitchF(:,2:end).^2 + ACCRollF(:,2:end).^2 + ACCYawF(:,2:end).^2)];
+    
+    GYR = [GYRPitch(:,1), sqrt(GYRPitch(:,2:end).^2 + GYRRoll(:,2:end).^2 + GYRYaw(:,2:end).^2)];
+    GYRF = [GYRPitch(:,1), sqrt(GYRPitchF(:,2:end).^2 + GYRRollF(:,2:end).^2 + GYRYawF(:,2:end).^2)];
     
     % Kistler
     % Acc1 = [Acc1, sqrt(Accx1.^2 + Accy1.^2 + Accz1.^2)];
@@ -97,8 +99,8 @@ for k = 1: length(Trial)
     [HeelContactInstant] = Instants(instant,File);
     HeelContact = floor(HeelContactInstant*Fs);
     
-    % Building inputs for Orthogonal Least Squares Algorithm
-    % (ols.m) implemented by Renato Naville Watanabe
+    % --- Building inputs for Orthogonal Least Squares Algorithm
+    % --- (ols.m) implemented by Renato Naville Watanabe
     for j = 1 : length(Sensors)
         first(j) = HeelContact(j,1);
         last(j) =  HeelContact(j,1)+floor(Fs);
@@ -107,13 +109,21 @@ for k = 1: length(Trial)
             
             p(:,i,k+(j-1)*length(Trial)) = eval([Var{i},'(first:last,eval(Sensors{j}))']);
             
-            stimulWin = exp(-0.5*((Acc(:,1)-(HeelContactInstant(1,2)-50e-3))/(50e-3/3)).^2);
+            stimulWin = exp(-0.5*((ACC(:,1)-(HeelContactInstant(1,2)-50e-3))/(50e-3/3)).^2);
             
             y(:,k+(j-1)*length(Trial)) = stimulWin(first:last);
             
             
         end
     end
+
+%     HeelContact = ceil(HeelContactInstant*FsFP);
+%     for j = 1: length(Sensors)
+%         first1(j) = HeelContact(j,1);
+%         last1(j) =  HeelContact(j,1)+floor(FsFP);
+%     end
+%     figure(k);
+%     subplot(2,1,1); plot(Fy(first1(1):last1(1),1), Fy(first1(1):last1(1),2:end));
 end
 
 beta = ols(p,y); % Coefs for linear combination
@@ -122,28 +132,23 @@ beta = ols(p,y); % Coefs for linear combination
 LinearCombination = zeros(ceil(Fs),1);
 for i = 1:length(Var)
     for j = 1 : length(Sensors)*length(Trial)
-        LinearCombination = LinearCombination + beta(i,j)*p(:,i,j); 
+        LinearCombination = LinearCombination + beta(i,j)*p(:,i,j);
     end
 end
 
- %%
- Fs1 = 300;
- 
-HeelContact = floor(HeelContactInstant*Fs1);
-for j = 1: length(Sensors)
- first(j) = HeelContact(j,1);
- last(j) =  HeelContact(j,1)+floor(Fs1);
-end
-figure;
-subplot(2,1,1); plot(Fy(first(1):last(1),1), Fy(first(1):last(1),2:end));
-subplot(2,1,2); plot(LinearCombination);
- 
+%%
+
+% for i = 1:length(Trial)
+% figure(i)
+% subplot(2,1,2); plot(LinearCombination);
+% end
+
 %% -- Plots
-% % % % 
+% % % %
 % % figure;
 % % subplot(3,1,1); plot(Fy(:,1), Fy(:,2:end));
 % % legend({'1','2','3','4','5','6','7'})
-% % 
+% %
 % % %Delsys
 % % subplot(3,1,2); plot(Acc(:,1), Acc(:,ShankL),AccF(:,1), AccF(:,ShankL)); ylabel('Shank L')
 % % title('Resultant'); legend('Raw','Filtered')
@@ -154,12 +159,12 @@ subplot(2,1,2); plot(LinearCombination);
 % % %subplot(3,1,3); plot(Acc2(:,1),Acc2(:,ShankL)); ylabel('Shank R')
 % % ylim([-2 2]);
 % % PlotInstants( instant, File )
-% % 
+% %
 % % %--
 % % figure;
 % % subplot(3,1,1)
 % % plot(Fy(:,1), Fy(:,2:end)); legend({'1','2','3','4','5','6','7'})
-% % 
+% %
 % % subplot(3,1,2); plot(Acc(:,1), Pitch(:,ShankL),Acc(:,1), PitchF(:,ShankL)); ylabel('Shank L')
 % % title('Pitch'); legend('Raw','Filtered')
 % % subplot(3,1,3); plot(Acc(:,1), Pitch(:,ShankR),AccF(:,1), PitchF(:,ShankR)); ylabel('Shank R')
@@ -184,12 +189,12 @@ subplot(2,1,2); plot(LinearCombination);
 % % subplot(3,1,3); plot(Acc(:,1), Yaw(:,ShankR), AccF(:,1), YawF(:,ShankR));
 % % ylabel('Shank R');ylim([-2 2]);
 % % PlotInstants( instant, File )
-% % 
+% %
 % % %--
 % % X = Acc; XF = AccF;
 % % X(:,2:end) = Acc(:,2:end).^2 - Acc(:,2:end);
 % % XF(:,2:end) = AccF(:,2:end).^2 - AccF(:,2:end);
-% % 
+% %
 % % figure;
 % % subplot(3,1,1)
 % % plot(Fy(:,1), Fy(:,2:end)); legend({'1','2','3','4','5','6','7'})
@@ -199,5 +204,3 @@ subplot(2,1,2); plot(LinearCombination);
 % % subplot(3,1,3); plot(Acc(:,1), X(:,ShankR), Acc(:,1), XF(:,ShankR));
 % % ylabel('Shank R');ylim([-1 10]);
 % % PlotInstants( instant, File )
-
-%%
